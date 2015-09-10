72da4440b0ec36b49778c3cb9d000117599dae17
Daniel Gimenes <danielcgimenes@gmail.com>
2015-09-10 01:08:18 -0300

    changing name and package

--- app/src/main/java/br/com/dgimenes/popmovies/DetailsActivity.java
+++ app/src/main/java/br/com/dgimenes/popmovies/DetailsActivity.java
@@ -1,23 +1,22 @@
-package trendingmovies.dgimenes.com.br.trendingmovies;
+package br.com.dgimenes.popmovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class DetailsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_details);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_details, menu);
         return true;
     }
 
